556e1ee03f066c2161fd14f3549f0a638bc92db3
karthik <kmalugu.official@gmail.com>
2024-02-21 10:09:29 +0530

    Initial commit

--- day1of45.py
+++ day1of45.py
@@ -5,3 +5,12 @@ x = "hello"
 
 #if condition is false, then we can print our output as AssertionError
 assert x == "goodbye","x should be hello"
+
+
+
+#raise keyword is used to raise an Exception
+
+x = "hello"
+
+if not type(x) is int:
+    raise TypeError("x should be an integer")
