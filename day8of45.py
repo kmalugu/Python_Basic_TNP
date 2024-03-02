2ebc248bdf9fc98cf0405c2214d9a64dfc3815b7
karthik <kmalugu.official@gmail.com>
2024-03-02 10:47:12 +0530

    Initial commit

--- day8of45.py
+++ day8of45.py
@@ -3,6 +3,43 @@
 # printer(1,2,3,"hello")
 
 
-def printer(*args, **kwargs):
-    print(f"args: {args} \nkargs: {kwargs}")
-printer(1,2,3,name="Karthik", age=21, dept="CSE")
+# def printer(*args, **kwargs):
+#     print(f"args: {args} \nkargs: {kwargs}")
+# printer(1,2,3,name="Karthik", age=21, dept="CSE")
+
+
+# class Animal:
+#     def shout(self):
+#         print("Animal Shouting")
+# class Dog(Animal):
+#     def bark(self):
+#         print("Dog Barking")
+#
+# d = Dog()
+# d.bark()
+# d.shout()
+
+
+
+
+# Multi level Inheritance
+
+class Animal:
+    def shout(self):
+        print("Animal Shouting")
+
+class Dog(Animal):
+    def bark(self):
+        print("Dog Barking")
+
+class Dog_breed(Dog):
+    def breed(self):
+        print("Breed Labrador")
+
+    def bark(self):
+        print("Labrador Barking")
+
+d = Dog_breed()
+d.breed()
+d.bark()
+d.shout()
